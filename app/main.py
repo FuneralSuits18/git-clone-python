9aff6f1a794b6ff1ed0150bafca8eade5d8b5c84
FuneralSuits18 <mahirtahmid@gmail.com>
2024-06-29 19:15:12 +0600

    Implement reading tree objects

--- app/main.py
+++ app/main.py
@@ -21,9 +21,39 @@ def create_blob():
         print(hash_value.strip("\n"), end="")
         if sys.argv[2] == "-w":
             if not os.path.isdir(f".git/objects/{hash_value[:2]}"):
-                 os.mkdir(f".git/objects/{hash_value[:2]}")
+                os.mkdir(f".git/objects/{hash_value[:2]}")
             with open(f".git/objects/{hash_value[:2]}/{hash_value[2:]}", "wb") as blob:
                 blob.write(zlib.compress(bytes(f"{header}\0{content}", "utf-8")))
+
+def read_tree():
+    tree_sha = sys.argv[3]
+    with open(f".git/objects/{tree_sha[:2]}/{tree_sha[2:]}", "rb") as file:
+        tree = zlib.decompress(file.read())
+        header, next_content = tree.split(b"\x00", maxsplit=1)
+        while next_content:
+            mode, after_mode = next_content.split(b" ", maxsplit=1)
+            name, after_name = after_mode.split(b"\x00", maxsplit=1)
+            sha = after_name[:20]
+            next_content = after_name[20:]
+            mode_name = "blob"
+            if mode == b"100644":       # regular file
+                mode = "100644"
+            elif mode == b"100755":     # executable file
+                mode = "100755"
+            elif mode == b"120000":     # symbolic link
+                mode = "120000"
+            elif mode == b"040000":     # directory
+                mode = "040000"
+                mode_name = "tree"
+            else:                       # gitlink / submodule (repos nested in other repos)
+                mode = "160000"
+                mode_name = "submodule"
+            if sys.argv[2] == "--name-only":        # only this flag is implemented
+                print(name.decode("utf-8"))
+            else:
+                print(f"{mode} {mode_name} {sha.hex()}    {name.decode("utf-8")}")
+
+
 
 
 def main():
@@ -39,6 +69,8 @@ def main():
         read_blob()
     elif command == "hash-object":
         create_blob()
+    elif command == "ls-tree":
+        read_tree()
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
